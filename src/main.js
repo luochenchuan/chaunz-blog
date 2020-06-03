5f53c875af67f8fc07c9d617ea3f26c947d14e80
chuansheng <2635906359@qq.com>
2020-06-03 12:37:20 +0800

    项目初始化, 整合 element-ui组件

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUi from 'element-ui'
 
+// 添加 element-ui组件
+import ElementUi from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
