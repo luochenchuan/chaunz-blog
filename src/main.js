1a3698c85410974a151d9395027013abc5608c21
chuansheng <zhuchuansheng@ict.ac.cn.com>
2020-07-01 17:47:10 +0800

    删除element-ui, 使用 ant-vue, 使用 yarn start 启动

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Antd from 'ant-design-vue'
+
 import App from './App'
 import router from './router'
-
-// 添加 element-ui组件
-import ElementUi from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import 'ant-design-vue/dist/antd.css'
 
 Vue.config.productionTip = false
-Vue.use(ElementUi)
+Vue.use(Antd)
 
 /* eslint-disable no-new */
 new Vue({
